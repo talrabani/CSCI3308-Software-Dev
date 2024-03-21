9d27afd5c46235c10c3e751084aa1b7aedf33c78
talrabani <111509210+talrabani@users.noreply.github.com>
2024-03-21 14:41:00 -0600

    Inserted Data for SPORTS and BROKERS

---
+++ src/init_data/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO TABLE sports (sport_name) VALUES (
+    'Football',
+    'Basketball',
+    'Baseball',
+    'Hockey',
+    'Soccer',
+    'Tennis',
+    'Golf',
+    'Boxing',
+    'UFC',
+    'Cricket');
+
+INSERT INTO TABLE brokers (broker_name) VALUES (
+    'Bet365',
+    'William Hill',
+    'Betfair',
+    'Paddy Power',
+    'Ladbrokes',
+    'Betfred',
+    'Coral',
+    'Unibet',
+    '888sport',
+    'BetVictor',
+    'DraftKings');
